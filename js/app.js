9d527300e0609e6565eb7931ea034490419ae6f4
Your Name <anishThomas4419@gmail.com>
2018-04-19 23:42:27 +0530

    Combobox using angularjs and bootsrap

---
+++ js/app.js
@@ -0,0 +1,37 @@
+(function(){
+	var appModule = angular.module('autoCompleteApp', []);
+	
+	appModule.controller('autoCompleteAppCtrl', function($scope){
+		$scope.name = "anish";
+		$scope.hideList = true;
+		$scope.countryList = [
+			"apple", "orange", "jackfruit"
+		];
+		
+		$scope.selectedCountry = [];
+		
+		$scope.complete = function(countryValue){
+			$scope.hideList= false;
+			var filteredList = [];
+			angular.forEach($scope.countryList, function(countryName){
+				if(countryName.toLowerCase().toString().indexOf(countryValue.toLowerCase()) >= 0){
+					filteredList.push(countryName)
+				}
+			})
+			if(filteredList.length == 0){
+				filteredList.push("No Results Found")
+			}
+			$scope.selectedCountry = filteredList;
+		}
+		
+		$scope.assignSelected = function(selectedCountry){
+			$scope.hideList= true;
+			$scope.country = selectedCountry;
+		}
+		
+		$scope.hidelist = function(){
+			$scope.hideList = true;
+		}
+		
+	});
+})()
